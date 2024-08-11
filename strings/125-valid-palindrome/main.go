33dead0e297d98d32febd15166ebf3bab4e3f397
Vitor Reis <reis_vitor@outlook.com>
2024-08-11 16:26:52 -0300

    feat: implementation of func isPalindrome

--- strings/125-valid-palindrome/main.go
+++ strings/125-valid-palindrome/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
-func main(){
+func main() {
 	fmt.Println("Hello, world!")
+}
+
+func removeNonAlphanumeric(input string) string {
+	re := regexp.MustCompile("[^a-zA-Z0-9]+")
+	return re.ReplaceAllString(input, "")
+}
+
+func isPalindrome(word string) bool {
+	alphaNumeric := removeNonAlphanumeric(word)
+	lower := strings.ToLower(alphaNumeric)
+
+	if len(lower) == 0 {
+		return true
+	}
+
+	for i, j := 0, len(lower)-1; i < j; i, j = i+1, j-1 {
+		if lower[i] != lower[j] {
+			return false
+		}
+	}
+
+	return true
 }
